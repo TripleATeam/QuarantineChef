dfe6158e1e165bef811e4fe44e57d1cb5418b5d8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 13:48:10 -0700

    Move to src

--- database/IngredientGroup.java
+++
@@ -1,5 +0,0 @@
-public enum IngredientGroup {
-    DAIRY,
-    GRAINS,
-    ETC
-}
--- database/UserProfile.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.lang.*;
-
-public class UserProfile {
-    //public String username;
-    public int[] preferences;
-    public int[] health;
-    public int[] diet;
-}
