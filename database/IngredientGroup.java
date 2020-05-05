3f8757db371ecbe57bc9a78877925dedcc346693
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 13:37:14 -0700

    Started bare-bones project and added Maven.

---
+++ database/IngredientGroup.java
@@ -0,0 +1,5 @@
+public enum IngredientGroup {
+    DAIRY,
+    GRAINS,
+    ETC
+}
---
+++ database/UserProfile.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+
+public class UserProfile {
+    //public String username;
+    public int[] preferences;
+    public int[] health;
+    public int[] diet;
+}
