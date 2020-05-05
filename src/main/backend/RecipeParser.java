13b8b5bc232e4400d440a71e3f5f3f4234ddec23
Michael Figgs <mfiggs@uw.edu>
2020-05-05 14:32:56 -0700

    Added backend class files

---
+++ src/main/backend/RecipeParser.java
@@ -0,0 +1,23 @@
+/*
+    Class to interpret json file from the api
+    TODO
+ */
+
+// TODO
+public class RecipeParser {
+    // Fields
+    List<Recipe> recipeList;
+
+    public RecipeParser() {
+        // TODO
+    }
+
+    private void parse() {
+        // TODO
+    }
+
+    // TODO
+    public List<Recipe> getRecipeList() {
+        return recipeList;
+    }
+}
