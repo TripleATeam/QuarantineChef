42bbacd6d4e81109d245db985add19377ae66882
BuildTools <unconfigured@null.spigotmc.org>
2020-05-05 15:00:45 -0700

    Fixed path.

--- src/main/backend/Ingredient.java
+++
@@ -1,9 +0,0 @@
-/*
-    Ingredient object
- */
-
-public class Ingredient {
-    String name;
-    int amount;
-    String date; // how do we want to do this??
-}
--- src/main/backend/Recipe.java
+++
@@ -1,9 +0,0 @@
-/*
-    Recipe object
- */
-
-public class Recipe {
-    List<Ingredient> ingredientList;
-    String imageUrl;
-    String recipeUrl;
-}
