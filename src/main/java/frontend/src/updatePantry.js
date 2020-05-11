3c0b9fbff3ae8326e080fd3e3cf54ae17a4fabea
kylematthewjohnson <johnsk01@uw.edu>
2020-05-11 16:43:22 -0700

    update UpdatePantry

--- src/main/java/frontend/src/updatePantry.js
+++ src/main/java/frontend/src/updatePantry.js
@@ -30,43 +30,31 @@ export default function UpdatePantry(props) {
             }
         }
 
-        // for (let i = 0; i < master.length; i++) {
-        //     console.log(props.pantry.get(master[i]));
-        //     if (props.pantry.get(master[i])) {
-        //         ing.push(master[i]);
-        //         exp.push(props.expiration.get(master[i]));
-        //         qnt.push(1);
-        //     }
-        // }
-
         let pantry = {
             ingredients: ing,
             expirations: exp,
             quantities: qnt,
-          };
+        };
 
-          const requestOptions = {
+        const requestOptions = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(pantry),
-          };
-      
-          fetch("http://localhost:4567/save-pantry", requestOptions);
-      
-         
-        
+        };
+
+        fetch("http://localhost:4567/save-pantry", requestOptions);
 
         function formatDate(date) {
             var d = new Date(date),
                 month = '' + (d.getMonth() + 1),
                 day = '' + d.getDate(),
                 year = d.getFullYear();
-        
-            if (month.length < 2) 
+
+            if (month.length < 2)
                 month = '0' + month;
-            if (day.length < 2) 
+            if (day.length < 2)
                 day = '0' + day;
-        
+
             return [day, month, year].join('-');
         }
     }
