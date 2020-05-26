65006f98dec77ebfd65ce1325833fb3731770f7e
Julia Kalmykov <jkalm@cs.washington.edu>
2020-05-25 18:09:22 -0700

    Sign out button

--- src/main/java/frontend/src/SignIn.js
+++ src/main/java/frontend/src/SignIn.js
@@ -1,36 +1,54 @@
 import React, {Component} from 'react';
+import Button from "@material-ui/core/Button";
 
 class SignInComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 0};
+        this.state = {isSignedIn: false, userName: "", googleUserId: ""};
         window.handleSignIn = this.handleSignIn.bind(this);
+        this.handleSignOut = this.handleSignOut.bind(this);
     }
 
-     async handleSignIn(event) {
-        console.log("user: " + event);
-         var profile = event.getBasicProfile();
-         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-         console.log('Name: ' + profile.getName());
-         console.log('Image URL: ' + profile.getImageUrl());
-         console.log('Email: ' + profile.getEmail());
-         this.setCookie('userId', profile.getId(), 1);
-     }
+    async handleSignIn(googleUser) {
+        const profile = googleUser.getBasicProfile();
+        this.setState({
+            isSignedIn: true,
+            userName: profile.getName(),
+            googleUserId: profile.getId()
+        });
+        this.setCookie('googleUserId', this.state.googleUserId, 30);
+        console.log('Signed in.', this.state);
+    }
 
-     setCookie(name,value,days) {
+    setCookie(name, value, days) {
         var expires = "";
         if (days) {
             var date = new Date();
-            date.setTime(date.getTime() + (days*24*60*60*1000));
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    handleSignOut() {
+        const auth2 = window.gapi.auth2.getAuthInstance();
+        auth2.signOut().then((function () {
+            console.log('User signed out.');
+            this.setCookie('googleUserId', '', -1);
+            window.location.reload();
+        }).bind(this));
     }
 
     render() {
         return (
-        <div className="g-signin2" data-onsuccess="handleSignIn"></div>
+            <div>
+                {this.state.isSignedIn
+                    ? <div>Welcome {this.state.userName} <Button color="inherit" onClick={this.handleSignOut}>Sign Out</Button></div>
+                    : <div> <div className="g-signin2" data-onsuccess="handleSignIn"></div></div>
+                }
+            </div>
         );
     }
 }
+
 export default SignInComponent;
